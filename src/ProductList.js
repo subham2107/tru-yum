da65301a5b97b933bec62e8200f43e35aa5a9036
Subham Mitra <subham.mitra21@gmail.com>
2021-03-28 10:42:06 +0530

    product list

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,100 +1,105 @@
-import React from 'react';
-import ProductDetails from './ProductDetails';
-
-class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidMount() {
-      fetch(`/api/products/categories/${this.props.match.params.cartegoryname}`)
-        .then(response => response.json())
-        .then(response => {
-            console.log(response)
-            console.log("aaaa");
-            for(let i=0;i<(response).length;i++)
-           {
-               console.log((response[i].title));
-               const title = response[i].title;
-               <ProductDetails title = {title} quantity={response[i].quantity} />
-           }
-            this.setState({category: response});
-        });
-    }
-
-    render() {
-        return (
-          
-            <div>Product data: {JSON.stringify(this.state.category)}</div>
-        );
-    }
-}
-
-
 // import React from 'react';
+// import ProductDetails from './ProductDetails';
 
-// class DeveloperProfile extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {};
-//     }
 
-//     componentDidMount() {
-//         fetch(`/api/developers/${this.props.match.params.developerId}`)
+
+// class ProductList extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+
+//   componentDidMount() {
+//       fetch(`/api/products/categories/${this.props.match.params.cartegoryname}`)
 //         .then(response => response.json())
 //         .then(response => {
-//             this.setState({developer: response});
+//             //console.log(response)
+//             //console.log("aaaa");
+//             for(let i=0;i<(response).length;i++)
+//            {
+//                //console.log((response[i].title));
+//                const title = response[i].title;
+//                <ProductDetails title = {title} quantity={response[i].quantity} />
+//            }
+//             this.setState({category: response});
 //         });
 //     }
-
+    
 //     render() {
+
+//         // Category.find({}).then(category => {
+//         //     let categoryNames = category.map(function (cat) {
+//         //       return cat.name;
+//         //     });
+
+//         let categories= [];
+//     categories=(this.state.category);
+//         //console.log(this.state.category);
+//         console.log(`hellllllllllllo ${categories.title}`);
+//         // Category.find({}).then(category => {
+//         //          let categoryNames = categories.map(function (cat) {
+//         //            return cat.name;
+//         //         });
+
 //         return (
-//             <div>Developer data: {JSON.stringify(this.state.developer)}</div>
+            
+//             <div>
+//                Product data: {JSON.stringify(this.state.category)}
+//             </div>
 //         );
-//     }
+    
+//     // render() {
+//     //     return (
+            
+//     //         <div>Product data: {JSON.stringify(this.state.category)}</div>
+//     //     );
+//     // }
+// }
 // }
 
-// export default DeveloperProfile;
+//export default ProductList;
 
-//   onInput = event => {
-//     this.setState({ [event.target.name]: event.target.value });
-//   }
-  
-//   onUpdate = () => {
-//     const { firstName, lastName } = this.state;
-//     fetch('/api/users/me', {
-//       method: 'PUT',
-//       body: JSON.stringify({ firstName, lastName }),
-//       headers: {
-//         'Content-type': 'application/json; charset=UTF-8'
-//       }
-//     }).then(res => {
-//       if (res.status === 204) {
-//         window.location = '/profile';
-//       }
-//     });
-//   }
+import React from 'react';
+class ProductList extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        products: [],
+      };
+      this.getRandomUsers = this.getRandomUsers.bind(this);
+    }
+    async getRandomUsers() {
+      const res = await fetch(`/api/products/categories/${this.props.match.params.cartegoryname}`);
+      const data = await res.json();
+      console.log(data)
+      return data;
+    }
+    async componentDidMount() {
+      const products = await this.getRandomUsers();
+      console.log(products);
+      this.setState({ products });
 
-//   onLogout = () => {
-//     fetch('/api/sessions/me', {
-//       method: 'DELETE'
-//     }).then(res => {
-//       if (res.status === 204) {
-//         window.location = '/';
-//       }
-//     });
-//   }
+      console.log(this.state.products);
+      console.log(JSON.stringify(this.state.products));
+    }
+    render() {
+        return (
+            <div>
+              {(this.state.products).map((user) => (
+                  <h1>
+                  title={user.title}
+                  quantity={user.quantity}
+                  </h1>
+              ))}
+            </div>
+        );
+    }
+  }
 
-//   render() {
-//     return (<div className="Profile">
-//         <div>Email: {this.state.email}</div>
-//           <div>First Name: <input name="firstName" onInput={this.onInput} value={this.state.firstName || ''}></input></div>
-//           <div>Last Name: <input name="lastName" onInput={this.onInput} value={this.state.lastName || ''}></input></div>
-//           <div><input type="button" onClick={this.onUpdate} value="Update"></input></div>
-//           <div><input type="button" onClick={this.onLogout} value="Logout"></input></div>
-//       </div>);
-//   }
-// }
+  export default ProductList;
 
-export default ProductList;
+
+
+
+
+
