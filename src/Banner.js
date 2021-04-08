622a06fa57099a120599e8b8323da823839202bb
Subham Mitra <subham.mitra21@gmail.com>
2021-04-08 12:58:55 +0530

    logo improved

--- src/Banner.js
+++ src/Banner.js
@@ -8,9 +8,8 @@ function Banner() {
     <div className="App">
      <AliceCarousel autoPlay activeIndex infinite disableButtonsControls autoPlayInterval="3000">
       <img src="/images/2104007_summer-vegetables_460.jpg" className="sliderimg" alt=""/>
-      <img src="images/CXPN1-1200x300-25thmar21.jpg" className="sliderimg" alt=""/>
-      <img src="images/Organic-Staples-Banner-V2-1200x300.jpg" className="sliderimg" alt=""/>
       <img src="/images/Green_2104013_home-kitchen_460.jpg" className="sliderimg" alt=""/>
+      <img src="/images/2104008_melons-mela_460.jpg" className="sliderimg" alt=""/>
       <img src="/images/Green_2104015_dairy_460_SouthWest.jpg" className="sliderimg" alt=""/>
     </AliceCarousel>
     </div>
